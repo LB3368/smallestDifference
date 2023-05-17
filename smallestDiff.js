483e70f1f4ae13e1db2d50f345975a0719e521d5
Latisha Bridges <msbridges3368@gmail.com>
2023-05-17 10:37:05 -0400

    updated function making it simplier

--- smallestDiff.js
+++ smallestDiff.js
@@ -1,9 +1,24 @@
 const list1 = [10, 14, 16, 18, 20];
 const list2 = [23, 30, 33, 54, 96];
-  
-  const minDiff = smallestDiff(list1, list2);
+
+const minDiff = smallestDiff(list1, list2);
 
 function smallestDiff(list1, list2) {
+    let minDiff = Number.MAX_VALUE;
+    for (let i = 0; i < list1.length; i++) {
+      for (let j = 0; j < list2.length; j++) {
+        const diff = Math.abs(list1[i] - list2[j]);
+        if (diff < minDiff) {
+          minDiff = diff;
+        }
+      }
+    }
+    return minDiff;
+  }
+
+
+/*
+  function smallestDiff(list1, list2) {
     let minDiff = Number.MAX_VALUE;
     for (const num1 of list1) {
       for (const num2 of list2) {
@@ -15,6 +30,6 @@ function smallestDiff(list1, list2) {
     }
     return minDiff;
   }
-    
+    */
   
   console.log(minDiff)
